754180cc53b4d7261e2df73c5cc3f90695f3372e
GuilhermeMarqus <gjanuario403@gmail.com>
2025-03-28 16:58:23 -0300

    FEAT:(routes) initial routes created

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function Products() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      ROTEAMENTO DE PAGINAS
+      BEM VINDO AO PRODUCTS
       <Link href={"/home"}>HOME</Link>
-      <Link href={"/products"}>PRODUCTS</Link>
+      <Link href={"/"}>ROTEAMENTO</Link>
     </div>
   );
 }
