9d93e21f01f6551e2f8b2d0e4d04c334b3faf7d3
jotacarvalh0 <josecarvalho11235@gmail.com>
2025-03-31 16:50:13 -0300

    FIX:(layout) small correction

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react';
+
+interface LayoutProps {
+    children: ReactNode;
+}
+
+export default function RootLayout({ children }: LayoutProps) {
+    return (
+        <html lang="en">
+            <body>
+                {children}
+            </body>
+        </html>
+    );
+}
